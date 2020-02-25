abdb3efcf178f8a69646c8ce002652d357ee10f4
korChipmunk <rhj4862@gmail.com>
2020-02-25 12:06:09 +0900

    feat: Hello RestController 생성

---
+++ src/main/java/com/chipmunk/boot/springboot/web/HelloController.java
@@ -0,0 +1,14 @@
+package com.chipmunk.boot.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+}
