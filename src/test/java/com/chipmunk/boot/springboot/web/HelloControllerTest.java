37f9a8a8d6da3a22e433a84a0ab7ead22e33c4d4
korChipmunk <rhj4862@gmail.com>
2020-02-25 12:06:21 +0900

    feat: Hello RestController 테스트코드 생성

---
+++ src/test/java/com/chipmunk/boot/springboot/web/HelloControllerTest.java
@@ -0,0 +1,30 @@
+package com.chipmunk.boot.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello_returned() throws Exception {
+        String ret = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(ret));
+    }
+
+}
