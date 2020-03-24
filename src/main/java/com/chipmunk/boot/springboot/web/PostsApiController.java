220bf15d2ac63a43b3d273c92864d0b19b6b2fa8
korChipmunk <rhj4862@gmail.com>
2020-03-25 01:49:29 +0900

    feat: chapter 3 소스 코드

---
+++ src/main/java/com/chipmunk/boot/springboot/web/PostsApiController.java
@@ -0,0 +1,31 @@
+package com.chipmunk.boot.springboot.web;
+
+import com.chipmunk.boot.springboot.service.posts.PostsService;
+import com.chipmunk.boot.springboot.web.dto.PostsResponseDto;
+import com.chipmunk.boot.springboot.web.dto.PostsSaveRequestDto;
+import com.chipmunk.boot.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+    return postsService.findById(id);
+    }
+
+}
