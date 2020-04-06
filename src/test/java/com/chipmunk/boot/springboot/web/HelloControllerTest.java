bde71d56df71e248896db247b905effed38e3357
kor-Chipmunk <rhj4862@gmail.com>
2020-04-06 20:32:46 +0900

    feat: SecurityConfig 스캔 대상에서 제거

--- src/test/java/com/chipmunk/boot/springboot/web/HelloControllerTest.java
+++ src/test/java/com/chipmunk/boot/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.chipmunk.boot.springboot.web;
 
+import com.chipmunk.boot.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,12 +16,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+            excludeFilters = {
+            @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+            }
+)
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello_returned() throws Exception {
         String ret = "hello";
@@ -27,6 +36,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(ret));
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto_returned() throws Exception {
         String name = "hello";
