f9bca2c65ba743fb6799db9613217f0457a4cb24
kor-Chipmunk <rhj4862@gmail.com>
2020-05-01 02:53:31 +0900

    fix: 테스트 메소드명 한글로 변경

--- src/test/java/com/chipmunk/boot/springboot/web/HelloControllerTest.java
+++ src/test/java/com/chipmunk/boot/springboot/web/HelloControllerTest.java
@@ -28,7 +28,7 @@ public class HelloControllerTest {
 
     @WithMockUser(roles = "USER")
     @Test
-    public void hello_returned() throws Exception {
+    public void hello가_리턴된다() throws Exception {
         String ret = "hello";
 
         mvc.perform(get("/hello"))
@@ -38,7 +38,7 @@ public class HelloControllerTest {
 
     @WithMockUser(roles = "USER")
     @Test
-    public void helloDto_returned() throws Exception {
+    public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
 
